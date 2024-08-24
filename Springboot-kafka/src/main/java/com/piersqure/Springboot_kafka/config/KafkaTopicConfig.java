cc458831e9e65f0f24a064b5cac6cdb51e84369f
dvsnayak <yello.davis@gmail.com>
2024-08-24 18:27:36 +0530

    Kafka JSON producer , topic and controller created.

--- Springboot-kafka/src/main/java/com/piersqure/Springboot_kafka/config/KafkaTopicConfig.java
+++ Springboot-kafka/src/main/java/com/piersqure/Springboot_kafka/config/KafkaTopicConfig.java
@@ -1,15 +1,22 @@
 package com.piersqure.Springboot_kafka.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaTopicConfig {
 	
-	
+	@Bean
 	public NewTopic piersqureTopic() {
 		return TopicBuilder.name("piersqure")
+				.build();
+	}
+	
+	@Bean
+	public NewTopic piersqureJsonTopic() {
+		return TopicBuilder.name("piersqure_JSON")
 				.build();
 	}
 
